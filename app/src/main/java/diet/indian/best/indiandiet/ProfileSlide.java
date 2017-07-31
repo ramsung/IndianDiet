5a841ea8d2b176604abddfce076d5de9846fbbe3
RK <a.r.ramkumar25@gmail.com>
2017-07-31 19:44:29 +0530

    intro profile

--- app/src/main/java/diet/indian/best/indiandiet/ProfileSlide.java
+++ app/src/main/java/diet/indian/best/indiandiet/ProfileSlide.java
@@ -1,5 +1,12 @@
 package diet.indian.best.indiandiet;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
 import agency.tango.materialintroscreen.SlideFragment;
 
 /**
@@ -7,4 +14,33 @@ import agency.tango.materialintroscreen.SlideFragment;
  */
 
 public class ProfileSlide extends SlideFragment {
+    private CheckBox checkBox;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.user_prof_edit, container, false);
+        checkBox = (CheckBox) view.findViewById(R.id.checkBox);
+        return view;
+    }
+
+    @Override
+    public int backgroundColor() {
+        return R.color.green_800;
+    }
+
+    @Override
+    public int buttonsColor() {
+        return R.color.red_800;
+    }
+
+    @Override
+    public boolean canMoveFurther() {
+        return checkBox.isChecked();
+    }
+
+    @Override
+    public String cantMoveFurtherErrorMessage() {
+        return getString(R.string.error_message);
+    }
 }
